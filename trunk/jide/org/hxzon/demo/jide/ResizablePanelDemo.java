8d255d85fa5a765cc0f14b859c6532026a6b0363
xzonzh@gmail.com <xzonzh@gmail.com@a917fef6-57e1-4db9-a5c2-68c80a203731>
2011-09-02 06:33:13 +0000

    jide demo

---
+++ trunk/jide/org/hxzon/demo/jide/ResizablePanelDemo.java
@@ -0,0 +1,97 @@
+package org.hxzon.demo.jide;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+
+import com.jidesoft.swing.JideSwingUtilities;
+import com.jidesoft.swing.ResizablePanel;
+
+public class ResizablePanelDemo {
+    public static void createAndShowGUI() {
+        JFrame f = new JFrame();
+        f.setLayout(new BorderLayout());
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        demo(f);
+        f.pack();
+        JideSwingUtilities.globalCenterWindow(f);
+        f.setVisible(true);
+    }
+    
+    public static void demo(JFrame f){
+        final JPanel content = new JPanel();
+        content.setBackground(Color.blue);
+        JLabel label = new JLabel("hello world");
+        content.add(label);
+        content.setSize(100, 200);
+        ResizablePanel resizablePanel = new ResizablePanel();
+        resizablePanel.setLayout(new FlowLayout());
+        resizablePanel.setBackground(Color.red);
+//        resizePanel.setBorder(new EmptyBorder(4, 4, 4, 4) {
+//            @Override
+//            public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+//                super.paintBorder(c, g, x, y, width, height);
+//                ThemePainter painter = (ThemePainter) UIDefaultsLookup.get("Theme.painter");
+//                if (painter != null) {
+//                    Insets insets = getBorderInsets(c);
+//                    Rectangle rect = new Rectangle(0, y + height - insets.bottom, width, insets.bottom);
+//                    painter.paintGripper((JComponent) c, g, rect, SwingConstants.HORIZONTAL, ThemePainter.STATE_DEFAULT);
+//                }
+//            }
+//        });
+        resizablePanel.setBorder(new EmptyBorder(1,1,1,1));
+        resizablePanel.add(content);
+        JPanel p5 = new JPanel();
+        p5.setBackground(Color.green);
+        p5.add(resizablePanel);
+        f.add(p5, BorderLayout.CENTER);
+        JPanel p1 = new JPanel();
+        JLabel label1 = new JLabel("1");
+        p1.add(label1);
+        JPanel p2 = new JPanel();
+        JLabel label2 = new JLabel("2");
+        p2.add(label2);
+        JPanel p3 = new JPanel();
+        JLabel label3 = new JLabel("3");
+        p3.add(label3);
+        JPanel p4 = new JPanel();
+        JLabel label4 = new JLabel("4");
+        p4.add(label4);
+        f.add(p1, BorderLayout.EAST);
+        f.add(p2, BorderLayout.SOUTH);
+        f.add(p3, BorderLayout.WEST);
+        f.add(p4, BorderLayout.NORTH);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    createAndShowGUI();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
