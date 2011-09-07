745b3a882a75f05d1eae6f80da5518b01b1f13ef
xzonzh@gmail.com <xzonzh@gmail.com@a917fef6-57e1-4db9-a5c2-68c80a203731>
2011-09-07 03:29:40 +0000

    jgraph-1.4.1.4 update to jgraph-1.7.1.7 (http://www.jgraph.com/jgraph.html)

--- trunk/jgraph/com/mxgraph/examples/swing/ClickHandler.java
+++ trunk/jgraph/com/mxgraph/examples/swing/ClickHandler.java
@@ -9,43 +9,44 @@ import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 
 public class ClickHandler extends JFrame {
-	private static final long serialVersionUID = -2764911804288120883L;
 
-	public ClickHandler() {
-		super("Hello, World!");
+    private static final long serialVersionUID = -2764911804288120883L;
 
-		final mxGraph graph = new mxGraph();
-		Object parent = graph.getDefaultParent();
+    public ClickHandler() {
+        super("Hello, World!");
 
-		graph.getModel().beginUpdate();
-		try {
-			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
-			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150, 80, 30);
-			graph.insertEdge(parent, null, "Edge", v1, v2);
-		} finally {
-			graph.getModel().endUpdate();
-		}
+        final mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
 
-		final mxGraphComponent graphComponent = new mxGraphComponent(graph);
-		getContentPane().add(graphComponent);
+        graph.getModel().beginUpdate();
+        try {
+            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
+            Object v2 = graph.insertVertex(parent, null, "World!", 240, 150, 80, 30);
+            graph.insertEdge(parent, null, "Edge", v1, v2);
+        } finally {
+            graph.getModel().endUpdate();
+        }
 
-		graphComponent.getGraphControl().addMouseListener(new MouseAdapter() {
+        final mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        getContentPane().add(graphComponent);
 
-			public void mouseReleased(MouseEvent e) {
-				Object cell = graphComponent.getCellAt(e.getX(), e.getY());
+        graphComponent.getGraphControl().addMouseListener(new MouseAdapter() {
 
-				if (cell != null) {
-					System.out.println("cell=" + graph.getLabel(cell));
-				}
-			}
-		});
-	}
+            public void mouseReleased(MouseEvent e) {
+                Object cell = graphComponent.getCellAt(e.getX(), e.getY());
 
-	public static void main(String[] args) {
-		ClickHandler frame = new ClickHandler();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 320);
-		frame.setVisible(true);
-	}
+                if (cell != null) {
+                    System.out.println("cell=" + graph.getLabel(cell));
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        ClickHandler frame = new ClickHandler();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 320);
+        frame.setVisible(true);
+    }
 
 }
