60d00b19f2475f6dfc33db8def96b97c75729cf9
xzonzh@gmail.com <xzonzh@gmail.com@a917fef6-57e1-4db9-a5c2-68c80a203731>
2011-09-15 09:47:51 +0000

    XYDatasetDemo

---
+++ trunk/jfreechart/org/hxzon/demo/jfreechart/XYDatasetDemo.java
@@ -0,0 +1,413 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2009, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------------
+ * BarChartDemo1.java
+ * ------------------
+ * (C) Copyright 2003-2009, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   ;
+ *
+ * Changes
+ * -------
+ * 09-Mar-2005 : Version 1 (DG);
+ *
+ */
+
+package org.hxzon.demo.jfreechart;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+
+import org.hxzon.swing.components.easy.HEasyJComboBox;
+import org.hxzon.swing.model.HEasyJModelValue;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.PolarPlot;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleInsets;
+import org.jfree.ui.RefineryUtilities;
+
+public class XYDatasetDemo extends ApplicationFrame {
+
+    private static final long serialVersionUID = 1L;
+    private static XYDataset dataset = createDataset();
+    private static JFreeChart timeSeriesChart = createTimeSeriesChart(dataset);
+    private static JFreeChart scatterChart = createScatterChart(dataset);
+    private static JFreeChart xyLineChart = createXYLineChart(dataset);
+    private static JFreeChart xyAreaChart = createXYAreaChart(dataset);
+    private static JFreeChart xyStepChart = createXYStepChart(dataset);
+    private static JFreeChart xyStepAreaChart = createXYStepAreaChart(dataset);
+
+    private static JFreeChart polarChart = createPolarChart(dataset);
+
+    public XYDatasetDemo(String title) {
+        super(title);
+        ChartPanel chartPanel = new ChartPanel(timeSeriesChart);
+        //have a bug after show tooltips
+        chartPanel.setHorizontalAxisTrace(true);
+        chartPanel.setVerticalAxisTrace(true);
+        chartPanel.setFillZoomRectangle(true);
+        chartPanel.setMouseWheelEnabled(true);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+        getContentPane().add(chartPanel);
+        getContentPane().add(new ChartComboBox(chartPanel), BorderLayout.SOUTH);
+    }
+
+    private static XYDataset createDataset() {
+
+        TimeSeries s1 = new TimeSeries("L&G European Index Trust");
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+
+        TimeSeries s2 = new TimeSeries("L&G UK Index Trust");
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s2);
+        dataset.addSeries(s1);
+
+        return dataset;
+    }
+
+    private static JFreeChart createTimeSeriesChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                true, // create legend?
+                true, // generate tooltips?
+                false // generate URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+            renderer.setDrawSeriesLineAsPath(true);
+        }
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MM-yyyy"));
+
+        return chart;
+    }
+
+    //散点图
+    private static JFreeChart createScatterChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createScatterPlot("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setNumberFormatOverride(new MyNumberFormat());
+
+        return chart;
+
+    }
+
+    private static JFreeChart createXYLineChart(XYDataset dataset) {
+    
+        JFreeChart chart = ChartFactory.createXYLineChart("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+    
+        chart.setBackgroundPaint(Color.white);
+    
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+    
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setNumberFormatOverride(new MyNumberFormat());
+    
+        return chart;
+    }
+
+    private static JFreeChart createXYAreaChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYAreaChart("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setNumberFormatOverride(new MyNumberFormat());
+
+        return chart;
+    }
+
+    private static JFreeChart createXYStepChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYStepChart("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MM-yyyy"));
+
+        return chart;
+    }
+
+    private static JFreeChart createXYStepAreaChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYStepAreaChart("Legal & General Unit Trust Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setNumberFormatOverride(new MyNumberFormat());
+
+        return chart;
+    }
+
+    private static JFreeChart createPolarChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createPolarChart("Legal & General Unit Trust Prices", // chart title
+                dataset, // data
+                true, // include legend
+                true, // tooltips?
+                false // URLs?
+                );
+
+        chart.setBackgroundPaint(Color.white);
+
+        PolarPlot plot = (PolarPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        //        plot.setDomainGridlinePaint(Color.white);
+        //        plot.setRangeGridlinePaint(Color.white);
+        //        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        //        plot.setDomainCrosshairVisible(true);
+        //        plot.setRangeCrosshairVisible(true);
+
+        return chart;
+    }
+
+    public static class ChartComboBox extends HEasyJComboBox<JFreeChart> implements ItemListener {
+        private static final long serialVersionUID = 1L;
+        private ChartPanel chartPanel;
+
+        public ChartComboBox(ChartPanel chartPanel) {
+            this.chartPanel = chartPanel;
+            super.addItem(new HEasyJModelValue<JFreeChart>(timeSeriesChart, "TimeSeriesChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(scatterChart, "ScatterChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(xyLineChart, "XYLineChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(xyAreaChart, "XYAreaChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(xyStepChart, "XYStepLineChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(xyStepAreaChart, "XYStepAreaChart", false));
+            super.addItem(new HEasyJModelValue<JFreeChart>(polarChart, "PolarChart", false));
+            this.addItemListener(this);
+        }
+
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                @SuppressWarnings("unchecked")
+                JFreeChart item = ((HEasyJModelValue<JFreeChart>) e.getItem()).value;
+                chartPanel.setChart(item);
+            }
+
+        }
+    }
+
+    public static class MyNumberFormat extends NumberFormat {
+        private static final long serialVersionUID = 1L;
+        private static final double G = 1000 * 1000 * 1000;
+        private static final double M = 1000 * 1000;
+        private static final double K = 1000;
+
+        @Override
+        public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+            if (number >= G) {
+                return toAppendTo.append(number / G).append(" G");
+            } else if (number >= M) {
+                return toAppendTo.append(number / M).append(" M");
+            } else if (number >= K) {
+                return toAppendTo.append(number / K).append(" K");
+            }
+            return toAppendTo.append(number);
+        }
+
+        @Override
+        public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+            if (number >= G) {
+                return toAppendTo.append(number / G).append(" G");
+            } else if (number >= M) {
+                return toAppendTo.append(number / M).append(" M");
+            } else if (number >= K) {
+                return toAppendTo.append(number / K).append(" K");
+            }
+            return toAppendTo.append(number);
+        }
+
+        @Override
+        public Number parse(String source, ParsePosition parsePosition) {
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        XYDatasetDemo demo = new XYDatasetDemo("XY Dataset Chart Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+
+}
